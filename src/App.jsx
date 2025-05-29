32ffe91a3146feb79d9e9c261eb5b2f3151ebe29
Jakub <kuba2092001@interia.pl>
2025-05-29 21:54:17 +0200

    Add WordToGuess component to the page and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import GameStatus from './components/GameStatus';
 import Languages from './components/Languages';
+import WordToGuess from './components/WordToGuess';
 function App() {
+  const [currentWord, setCurrentWord] = useState('react');
+
   return (
     <main>
       <Header />
       <GameStatus />
       <Languages />
+      <WordToGuess currentWord={currentWord} />
     </main>
   );
 }
